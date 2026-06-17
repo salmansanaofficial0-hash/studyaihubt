b047d2fd54ee7c0460d94a13bb2be66de6d61395
salman sana <salmansanaofficial0@gmail.com>
2026-06-17 14:37:59 +0500

    __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,6 +104,18 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-25L1S1BCX4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-25L1S1BCX4');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
